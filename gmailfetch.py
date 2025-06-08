f607377d97e534caefff6715f7acfac6e7f16165
ikrogers <ikrogers92@gmail.com>
2025-06-08 07:17:47 -0700

    Add files via upload
    Python selenium script to manipulate webpages

--- gmailfetch.py
+++ gmailfetch.py
@@ -10,7 +10,7 @@ import base64
 import time
 import email
 import selenium
-import random
+import random as rand
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait as wait
@@ -23,6 +23,7 @@ import time
 from bs4 import BeautifulSoup, SoupStrainer
 
 # Define the SCOPES. If modifying it, delete the token.pickle file.
+#SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 def getEmails():
@@ -44,17 +45,15 @@ def getEmails():
 	# If credentials are not available or are invalid, ask the user to log in.
 	if not creds or not creds.valid:
 		if creds and creds.expired and creds.refresh_token:
-			print("penis")
 			creds.refresh(Request())
 		else:
       #you get the json from https://console.cloud.google.com/apis/credentials/oauthclient/648008577517-klkrk260il3jmijk123prrn5iesi77a7.apps.googleusercontent.com?project=gmailfetch-374220
-			flow = InstalledAppFlow.from_client_secrets_file('client_secret_648008577517-klkrk260il3jmijk123prrn5iesi77a7.apps.googleusercontent.com(2).json', SCOPES)
+			flow = InstalledAppFlow.from_client_secrets_file('client_secret_648008577517-o0eh00c85f1gef2d7k90mj7rqsgvps5i.apps.googleusercontent.com.json', SCOPES)
 			creds = flow.run_local_server(port=0)
 
 		# Save the access token in token.pickle file for the next run
 		with open('token.pickle', 'wb') as token:
 			pickle.dump(creds, token)
-
 
 	# Connect to the Gmail API
 	service = build('gmail', 'v1', credentials=creds)
@@ -63,7 +62,7 @@ def getEmails():
 	#result = service.users().messages().list(userId='me').execute()
 
 	# We can also pass maxResults to get any number of emails. Like this:
-	result = service.users().messages().list(maxResults=100, userId='me').execute()
+	result = service.users().messages().list(maxResults=500, userId='me').execute()
 	messages = result.get('messages')
 	#print(messages)
 	# messages is a list of dictionaries where each dictionary contains a message id.
@@ -99,10 +98,11 @@ def getEmails():
 			# it with BeautifulSoup library
 			#soup = BeautifulSoup(decoded_data , "lxml")
 			#body = soup.body()
-
+			#print("BEFORE IF STATEMENT")
 			# Printing the subject, sender's email and message
 			if subject=="You got bumped from the playlist. Spin again!":
-				#print("Subject: ", subject)
+
+				print("Subject: ", subject)
 				#print("From: ", sender)
 				# The Body of the message is in Encrypted format. So, we have to decode it.
 				# Get the data and decode it with base 64 decoder.
@@ -118,14 +118,17 @@ def getEmails():
 				# Now, the data obtained is in lxml. So, we will parse
 				# it with BeautifulSoup library
 				# do 'pip install lxml' or youll get parse error
+				print("before soup")
 				soup = BeautifulSoup(decoded_data , "lxml")
+				print("before soup")
 				body = soup.body()
 				#print("Message: ", body)
 				print('\n*********************END OF DATA**************************')
 
 				urls = re.findall('http://[a-zA-Z0-9_./?=-]*', str(body))
+				#print(urls)
 				options = Options()
-				options.add_argument("--user-data-dir=C:\\Users\\Ilya Rogers\\Documents\\udata")
+				options.add_argument("--user-data-dir=C:\\Users\\ikr\\Documents\\udata")
 				options.add_experimental_option("detach", True)
 				options.page_load_strategy = 'normal'
 				driver = webdriver.Chrome(options=options)
@@ -136,31 +139,39 @@ def getEmails():
 				selected = Select(driver.find_element(By.ID,"wheelSongId"))
 
 				# select by random index - crude implementation
-				list = [1,2,3,4,5,6,7,8,9]
-				selected.select_by_index(random.choice(list))
+				list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+				song = rand.choice(list)
+				print(song)
+				selected.select_by_index(song)
 			
 		
 
 				#Find span with onclick action and click it - Works commented out for testing
 				driver.find_element(By.XPATH, ".//span[contains(@onclick, 'connectWithSpotifyWheel();')]").click()
-				time.sleep(5)
-				driver.quit()
-				
-				#time.sleep(7200)
+				print("Sleeping then closing browser")
+				time.sleep(50)
+				driver.close()
+				break
+
+
 				#parse resulted html and go to the wheel link
 				#use selenium to step through autorization
 				#pick static song for now eventually read in whole list and pick randon one
 				#spin wheel till you cant
 				#remove email
-				#https://developers.google.com/gmail/api/reference/rest/v1/users.messages/trash
-				service.users().messages().trash(userId='me', id=msg['id']).execute()
+			else:
+				print(subject)
+				#print("Nothing Matching subjet found in last 50 emails")
 
 		except:
-			print("Exception thrown")
 			pass
 
+while 1:
+	getEmails()
+	sleeptime = rand.randrange(10000, 35000)
+	print("sleeping for: "+str(sleeptime))
+	time.sleep(sleeptime)
+	
+	
 
-
-#while True:
-getEmails()
 #TODO: Close all instances of chrome before starting another
